bde9376a1e14e8463541930a330b9a4ccf021708
xtony77 <xtony77@gmail.com>
2020-03-16 11:42:36 +0800

    - fix request header function name - add request get ip address

--- src/lampheart/Support/Http/Request.php
+++ src/lampheart/Support/Http/Request.php
@@ -162,7 +162,7 @@ trait Request
      *
      * @return array
      */
-    public function request_headers()
+    public function requestHeaders()
     {
         $headers = [];
 
@@ -175,5 +175,201 @@ trait Request
         }
 
         return $headers;
+    }
+
+    /**
+     * Fetch the IP Address
+     *
+     * @return	string
+     */
+    public function requestIP()
+    {
+        if (!empty($_SERVER["HTTP_CLIENT_IP"]))
+        {
+            $ip_address = $_SERVER["HTTP_CLIENT_IP"];
+        }
+        else if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
+        {
+            $ip_address = $_SERVER["HTTP_X_FORWARDED_FOR"];
+        }
+        else if (!empty($_SERVER['REMOTE_ADDR']))
+        {
+            $ip_address = $_SERVER["REMOTE_ADDR"];
+        }
+        else
+        {
+            $ip_address = '0.0.0.0';
+        }
+
+        if ( ! $this->validIP($ip_address))
+        {
+            $ip_address = '0.0.0.0';
+        }
+
+        return $ip_address;
+    }
+
+    /**
+     * Validate IP Address
+     *
+     * @access	public
+     * @param	string
+     * @param	string	ipv4 or ipv6
+     * @return	bool
+     */
+    public function validIP($ip, $which = '')
+    {
+        $which = strtolower($which);
+
+        // First check if filter_var is available
+        if (is_callable('filter_var'))
+        {
+            switch ($which) {
+                case 'IPv4':
+                    $flag = FILTER_FLAG_IPV4;
+                    break;
+                case 'IPv6':
+                    $flag = FILTER_FLAG_IPV6;
+                    break;
+                default:
+                    $flag = '';
+                    break;
+            }
+
+            return (bool) filter_var($ip, FILTER_VALIDATE_IP, $flag);
+        }
+
+        if ($which !== 'IPv6' && $which !== 'IPv4')
+        {
+            if (strpos($ip, ':') !== FALSE)
+            {
+                $which = 'IPv6';
+            }
+            elseif (strpos($ip, '.') !== FALSE)
+            {
+                $which = 'IPv4';
+            }
+            else
+            {
+                return FALSE;
+            }
+        }
+
+        $func = '_valid'.$which;
+        return $this->$func($ip);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Validate IPv4 Address
+     *
+     * Updated version suggested by Geert De Deckere
+     *
+     * @access	protected
+     * @param	string
+     * @return	bool
+     */
+    protected function _validIPv4($ip)
+    {
+        $ip_segments = explode('.', $ip);
+
+        // Always 4 segments needed
+        if (count($ip_segments) !== 4)
+        {
+            return FALSE;
+        }
+        // IP can not start with 0
+        if ($ip_segments[0][0] == '0')
+        {
+            return FALSE;
+        }
+
+        // Check each segment
+        foreach ($ip_segments as $segment)
+        {
+            // IP segments must be digits and can not be
+            // longer than 3 digits or greater then 255
+            if ($segment == '' OR preg_match("/[^0-9]/", $segment) OR $segment > 255 OR strlen($segment) > 3)
+            {
+                return FALSE;
+            }
+        }
+
+        return TRUE;
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Validate IPv6 Address
+     *
+     * @access	protected
+     * @param	string
+     * @return	bool
+     */
+    protected function _validIPv6($str)
+    {
+        // 8 groups, separated by :
+        // 0-ffff per group
+        // one set of consecutive 0 groups can be collapsed to ::
+
+        $groups = 8;
+        $collapsed = FALSE;
+
+        $chunks = array_filter(
+            preg_split('/(:{1,2})/', $str, NULL, PREG_SPLIT_DELIM_CAPTURE)
+        );
+
+        // Rule out easy nonsense
+        if (current($chunks) == ':' OR end($chunks) == ':')
+        {
+            return FALSE;
+        }
+
+        // PHP supports IPv4-mapped IPv6 addresses, so we'll expect those as well
+        if (strpos(end($chunks), '.') !== FALSE)
+        {
+            $ipv4 = array_pop($chunks);
+
+            if ( ! $this->_validIPv4($ipv4))
+            {
+                return FALSE;
+            }
+
+            $groups--;
+        }
+
+        while ($seg = array_pop($chunks))
+        {
+            if ($seg[0] == ':')
+            {
+                if (--$groups == 0)
+                {
+                    return FALSE;	// too many groups
+                }
+
+                if (strlen($seg) > 2)
+                {
+                    return FALSE;	// long separator
+                }
+
+                if ($seg == '::')
+                {
+                    if ($collapsed)
+                    {
+                        return FALSE;	// multiple collapsed
+                    }
+
+                    $collapsed = TRUE;
+                }
+            }
+            elseif (preg_match("/[^0-9a-f]/i", $seg) OR strlen($seg) > 4)
+            {
+                return FALSE; // invalid segment
+            }
+        }
+
+        return $collapsed OR $groups == 1;
     }
 }
